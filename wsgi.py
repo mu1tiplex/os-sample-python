23bd061170910fd633bffdf703daa3239236825d
Michael Peeters <michael@fuzzi.be>
2017-02-15 14:34:33 +0100

    Added random pause such that we can test probes.

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import time
+from random import randint
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+# sleep for a few seconds, to allow livelyness probe to fail sometimes 
+	sometime=randint(0,4) 
+	time.sleep(sometime)
     return "Hello NIS!"
 
 if __name__ == "__main__":
