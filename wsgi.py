c2457d6ecd60ce74ab666adda5e43e72b37e1626
Michael Peeters <michael@fuzzi.be>
2017-02-22 11:21:59 +0100

    Revert because of crash loop

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,15 @@
 from flask import Flask
-import time
-from random import randint
+#import time
+#from random import randint
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
 # sleep for a few seconds, to allow livelyness probe to fail sometimes 
-	sometime=randint(0,4) 
-	time.sleep(sometime)
-	return "Hello NIS! (with sleep)"
+#	sometime=randint(0,4) 
+#	time.sleep(sometime)
+	return "Hello NIS! (no sleep)"
 
 if __name__ == "__main__":
     application.run()
